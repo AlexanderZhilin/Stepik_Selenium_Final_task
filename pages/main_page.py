5263080712149673704a24d9abddb40a2dd94a4b
Александр Жилин <modelinterior@yandex.ru>
2022-09-29 22:41:03 +0300

    Feat: import login_page in test_main_page

--- pages/main_page.py
+++ pages/main_page.py
@@ -8,7 +8,7 @@ class MainPage(BasePage):  # MainPage является наследником к
     def go_to_login_page(self):
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
-        # time.sleep(5)
+         # time.sleep(5)
 
     def should_be_login_link(self):  # should_be_(название элемента) - метод проверки
         # assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented" - удали.устар
