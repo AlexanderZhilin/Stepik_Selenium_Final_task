6f7f176b45f22dae121abdb3541fb4e41d088a3b
Александр Жилин <modelinterior@yandex.ru>
2022-09-29 20:36:39 +0300

    Feat: should login link

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,15 +1,26 @@
+from selenium.common.exceptions import NoSuchElementException
+
+
 class BasePage():
 
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         """Конструктор класса.
         :param browser:
         :param url:
         """
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         """метод открывает нужную страницу,
         используя метод get()
         """
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):  # метод в котором будем перехватывать исключения.
+        try:
+            self.browser.find_element(how, what)  # Как искать (css, id, xpath и тд) и что искать (строку-селектор)
+        except (NoSuchElementException):
+            return False
+        return True
--- pages/main_page.py
+++ pages/main_page.py
@@ -2,8 +2,12 @@ from .base_page import BasePage  # из файла base_page.py импортир
 from selenium.webdriver.common.by import By
 
 
+# MainPage - это главная страница
 class MainPage(BasePage):  # MainPage является наследником класса BasePage
     def go_to_login_page(self):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
         # time.sleep(5)
+
+    def should_be_login_link(self):  # should_be_(название элемента) - метод проверки
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
